6b2284d0a14ba64b9ba7ba6d1319dcc1b00b830b
Angelina Litvinova <jeelybeenn@gmail.com>
2024-02-22 19:34:35 +0300

    init

---
+++ script.php
@@ -0,0 +1,94 @@
+<?php
+use Bitrix\Sale\Order;
+use Bitrix\Main\Loader;
+use \Bitrix\Main\SystemException;
+use \Bitrix\Main\UserTable;
+use \Bitrix\Main\Result;
+
+const USER_IDS = [1, 2];
+
+$result = transferOrders(USER_IDS);
+print_r($result);
+
+/**
+ * transferOrders
+ *
+ * @param  mixed $ids
+ * @throws \Bitrix\Main\SystemException
+ * @return array
+ */
+function transferOrders (array $ids): array {
+
+	if (!Loader::includeModule('sale')) {
+		throw new SystemException('Не удалось подключить модуль iblock.');
+	}
+
+	if (count($ids) > 2) {
+		throw new SystemException('Ошибка количества передаваемых идентификаторов. Максимальное число - 2.');
+	}
+
+	$userIds = checkUserIds($ids);
+	if (!empty($userIds)) {
+		return getOrders($userIds[0], $userIds[1]);
+	}
+
+}
+
+/**
+ * checkUserIds
+ *
+ * @param  mixed $ids
+ * @return array
+ */
+function checkUserIds (array $ids): array {
+	return array_column(UserTable::getList([
+		'select' => ['ID'],
+		'order' => ['ID' => 'ASC'],
+		'filter' => [
+			'=ID' => $ids,
+		],
+	])->fetchAll(), 'ID');
+}
+
+/**
+ * getOrders
+ *
+ * @param  mixed $from
+ * @param  mixed $to
+ * @throws \Bitrix\Main\SystemException
+ * @return array
+ */
+function getOrders(int $from, int $to): array {
+	$rsData = Order::getList([
+		'select' => ['ID'],
+		'filter' => [
+			"USER_ID" => $from,    
+		],
+		'order' => ['ID' => 'DESC'],
+		'limit' => 2,
+	]);
+
+	$result = new Result();
+
+	while ($arRow = $rsData->fetch()) {
+		$order = Order::load($arRow['ID']);
+
+		if (!$order) {
+			throw new SystemException('Не удалось получить заказ пользователя.');
+			return ['success' => false];
+		}
+
+        $order->setFieldNoDemand('USER_ID', $to);
+        $res = $order->save();
+
+        $result->addErrors($res->getErrors());
+        $result->addErrors($res->getWarnings());
+
+        return [
+            'errors' => $result->getErrorMessages(),
+            'success' => $result->isSuccess(),
+        ];
+	}
+
+	return ['success' => false];
+}
